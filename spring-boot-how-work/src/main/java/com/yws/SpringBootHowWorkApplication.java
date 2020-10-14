c60a4103d7e4ac371abcc4ca8cafcc49de0e24ec
yws <yws528819@163.com>
2020-10-14 08:01:54 +0800

    启动原理

---
+++ spring-boot-how-work/src/main/java/com/yws/SpringBootHowWorkApplication.java
@@ -0,0 +1,13 @@
+package com.yws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHowWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHowWorkApplication.class, args);
+    }
+
+}
