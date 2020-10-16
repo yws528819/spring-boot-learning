fb7a63754219257785a5d49ec67f4fecfcb5afd8
yws <yws528819@163.com>
2020-10-16 08:05:21 +0800

    cache原理

--- spring-boot-01-cache/src/main/java/com/yws/mapper/EmployeeMapper.java
+++ spring-boot-01-cache/src/main/java/com/yws/mapper/EmployeeMapper.java
@@ -8,12 +8,12 @@ public interface EmployeeMapper {
     @Select("select * from employee where id=#{id}")
     public Employee getEmpById(Integer id);
 
-    @Update("update employee set lastName=#{lastName}, email=#{email}, gender=#{gender}, d_Id=#{did} where id=#{id}")
+    @Update("update employee set lastName=#{lastName},email=#{email},gender=#{gender},d_Id=#{dId} where id=#{id}")
     public int updateEmp(Employee employee);
 
     @Delete("delete from employee where id=#{id}")
     public int deleteEmp(Integer id);
 
-    @Insert("insert into (lastName,email,gender,d_Id) values(#{lastName), #{email}, #{gender}, #{dId})")
+    @Insert("insert into employee (lastName,email,gender,d_Id) values(#{lastName}, #{email}, #{gender}, #{dId})")
     public int insertEmp(Employee employee);
 }
--- spring-boot-01-cache/src/main/java/com/yws/service/EmployeeService.java
+++ spring-boot-01-cache/src/main/java/com/yws/service/EmployeeService.java
@@ -3,6 +3,7 @@ package com.yws.service;
 import com.yws.bean.Employee;
 import com.yws.mapper.EmployeeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,47 @@ public class EmployeeService {
     @Autowired
     private EmployeeMapper employeeMapper;
 
+
+    /**
+     * 将方法的运行结果进行缓存；以后再要相同的数据，直接从缓存中获取，不用再调用方法；
+     * CacheManager管理多个Cache组件的，对缓存的真正CRUD操作在Cache组件中，每一个缓存组件有自己唯一的名字；
+     * 几个属性：
+     *      cacheNames/value：指定缓存组件的名字；
+     *      key：缓存数据使用的key；可以用它来指定。默认是使用方法的参数值 1-方法的返回值
+     *          编写SpEl；#id：参数id的值  还可以用如：#a0 #p0 #root.args[0]
+     *      keyGenerator：key的生成器；可以自己指定key的生成器的组件id
+     *          key/keyGenerator：二选一使用
+     *      cacheManager：指定缓存管理器；或者cacheResolver指定获取解析器
+     *      condition：指定符合条件的情况下才缓存；
+     *      unless：否定缓存；当unless指定的条件为true，方法的返回值就不会被缓存；可以获取到结果进行判断
+     *                  unless="#result == null"
+     *      sync：是否使用异步模式
+     *
+     * 原理：
+     *      1、自动配置类：CacheAutoConfiguration
+     *      2、缓存的配置类
+     *      org.springframework.boot.autoconfigure.cache.GenericCacheConfiguration
+     *      org.springframework.boot.autoconfigure.cache.JCacheCacheConfiguration
+     *      org.springframework.boot.autoconfigure.cache.EhCacheCacheConfiguration
+     *      org.springframework.boot.autoconfigure.cache.HazelcastCacheConfiguration
+     *      org.springframework.boot.autoconfigure.cache.InfinispanCacheConfiguration
+     *      org.springframework.boot.autoconfigure.cache.CouchbaseCacheConfiguration
+     *      org.springframework.boot.autoconfigure.cache.RedisCacheConfiguration
+     *      org.springframework.boot.autoconfigure.cache.CaffeineCacheConfiguration
+     *      org.springframework.boot.autoconfigure.cache.GuavaCacheConfiguration
+     *      org.springframework.boot.autoconfigure.cache.SimpleCacheConfiguration
+     *      org.springframework.boot.autoconfigure.cache.NoOpCacheConfiguration
+     *      3、哪个配置类默认生效：SimpleCacheConfiguration
+     *
+     *      4、给容器中注册了一个CacheManager：ConcurrentMapCacheManager  （有个ConcurrentMap属性放impleCache）
+     *      5、可以获取和创建ConcurrentMapCache类型的缓存组件；他的作用将数据保存在ConcurrentMap中；  （也有个ConcurrentMap放key-value数据）
+     *
+     * 运行流程：
+     *
+     * @param id
+     * @return
+     */
+    @Cacheable(cacheNames = {"emp"})
     public Employee getEmp(Integer id) {
         System.out.println("查询" + id + "号员工");
         Employee employee = employeeMapper.getEmpById(id);
